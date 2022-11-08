a1b5f51b36f94a4b7a97537c02f121783ae3239d
Mark Rattle <mark.rattle@gmail.com>
2022-11-08 18:42:07 -0500

    Revert "fixed a thing"
    This reverts commit 1ed4c4e5caf870c49314cfc008f88399e3856e48.
    
    Accidental key in comment

--- app.py
+++ app.py
@@ -246,7 +246,7 @@ def gif_search():
             'gifs': gifs
         }
 
-        # Uncomment me to see the result JSON!
+         # Uncomment me to see the result JSON!
         # Look closely at the response! It's a list
         # list of data. The media property contains a 
         # list of media objects. Get the gif and use it's 
@@ -254,7 +254,7 @@ def gif_search():
         # pp.pprint(gifs)
         return render_template('gif_search.html', **context)
     else:
-        return render_template('gif_search.html')
+        return render_template('gif_search.html', **context)
 
 if __name__ == '__main__':
     app.config['ENV'] = 'development'
